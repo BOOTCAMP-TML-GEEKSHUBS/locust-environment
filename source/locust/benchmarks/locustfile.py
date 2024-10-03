77797614c66298d2faa39c3023b659102d377cae
davidpestana <david.pestana.perdomo@gmail.com>
2024-10-03 11:07:56 +0200

    update

--- source/locust/benchmarks/locustfile.py
+++ source/locust/benchmarks/locustfile.py
@@ -1,67 +1,19 @@
-from locust import HttpLocust, TaskSet
-import json
+import time
+from locust import HttpUser, task, between
 
-def login(l):
-    data = {
-    }
-    l.client.post('/login', data=data)
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 5)
 
-def logout(l):
-    l.client.post("/logout")
-
-def getMovies100(l):
-    data = {
-    }
-    params = (
-         ('filter', '{"limit":100}'),
-    )
-    l.client.get('/api/Movies', data=data, params=params)
+    @task
+    def get_movies(self):
+        self.client.get("/api/Movies")
 
 
-def getMovies1000(l):
-    data = {
-    }
-    params = (
-         ('filter', '{"limit":1000}'),
-    )
-    l.client.get('/api/Movies', data=data, params=params)
+    # @task(3)
+    # def view_items(self):
+    #     for item_id in range(10):
+    #         self.client.get(f"/item?id={item_id}", name="/item")
+    #         time.sleep(1)
 
-def postMovie(l):
-    headers = {
-        "Authorization": "Bearer XXX", 
-        "Content-Type": "application/json" 
-    }
-
-    cookies = {
-        'Fuck-The-Cookie: XXXX': 'YYYY',
-    }
-    
-    params = (
-    )
-
-    payload = {
-        'title': 'movie title',
-        'overview': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-        'poster_path': 'https://es.web.img3.acsta.net/r_1280_720/pictures/18/02/21/10/05/2137559.jpg'
-    }
-
-    l.client.post('/api/Movies', data=json.dumps(payload), cookies=cookies, headers=headers)
-
-
-class UserBehavior(TaskSet):
-    tasks = {
-        getMovies100: 1,
-        getMovies1000: 1, 
-        postMovie: 10
-    }
-
-    # def on_start(self):
-    #     login(self)
-
-    # def on_stop(self):
-    #     logout(self)
-
-class WebsiteUser(HttpLocust):
-    task_set = UserBehavior
-    min_wait = 5000
-    max_wait = 9000
+    def on_start(self):
+        self.client.post("/api/Movies", json={ "title": "string", "overview": "string","poster_path": "string" })
